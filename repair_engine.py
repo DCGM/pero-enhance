5d2606bda17f96e4e5ad67af07e29ffbdfa298e0
ikodym <oldrich.kodym@gmail.com>
2019-12-18 13:52:45 +0100

    added document enhancement engine, user script and interactive demo

---
+++ repair_engine.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+import shutil
+import time
+import os
+import argparse
+import re
+import pickle
+import json
+
+from pero.document_ocr import crop_engine
+
+import numpy as np
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import tensorflow as tf
+
+def printProgressBar (iteration, total, prefix='', suffix='', decimals=1, length=20, fill='█', printEnd="\r"):
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end=printEnd)
+    if iteration == total:
+        print()
+
+class EngineRepairCNN(object):
+    def __init__(self, json_path, use_cpu=False):
+        parent_folder = os.path.dirname(json_path)
+        with open(json_path, 'r', encoding='utf8') as f:
+            config = json.load(f)
+        with open(os.path.join(parent_folder, config['chars_path']), 'rb') as handle:
+            rb = pickle.load(handle)
+        self.from_char = rb['from_char']
+        self.to_char = rb['to_char']
+        self.chars = rb['chars']
+        self.max_labels = config['max_labels']
+        self.height = config['height']
+        self.max_width = config['max_width']
+
+        repair_model = config['repair_model']
+        inpainting_model = config['inpainting_model']
+
+        self.cropper = crop_engine.EngineLineCropper(line_height=config['height'], poly=2, scale=1)
+
+        if repair_model:
+            tf.reset_default_graph()
+            saver = tf.train.import_meta_graph(os.path.join(parent_folder, repair_model) + '.meta')
+            if use_cpu:
+                tf_config = tf.ConfigProto(device_count={'GPU': 0})
+            else:
+                tf_config = tf.ConfigProto(device_count={'GPU': 1})
+                tf_config.gpu_options.allow_growth = True
+            self.repair_session = tf.Session(config=tf_config)
+            saver.restore(self.repair_session, os.path.join(parent_folder, repair_model))
+
+        if inpainting_model:
+            tf.reset_default_graph()
+            saver = tf.train.import_meta_graph(os.path.join(parent_folder, inpainting_model) + '.meta')
+            if use_cpu:
+                tf_config = tf.ConfigProto(device_count={'GPU': 0})
+            else:
+                tf_config = tf.ConfigProto(device_count={'GPU': 1})
+                tf_config.gpu_options.allow_growth = True
+            self.inpainting_session = tf.Session(config=tf_config)
+            saver.restore(self.inpainting_session, os.path.join(parent_folder, inpainting_model))
+
+    def enhance_page(self, page_img, page_layout):
+        num_lines = 0
+        for _ in page_layout.lines_iterator():
+            num_lines += 1
+
+        for i, textline in enumerate(page_layout.lines_iterator(), 1):
+            page_img = self.enhance_line_in_page(page_img, textline)
+            printProgressBar(i, num_lines, prefix='Repairing line {} ({}/{})'.format(textline.id, i, num_lines))
+
+        return page_img
+
+    def enhance_line_in_page(self, page_img, textline):
+        line_crop, line_mapping, offset = self.cropper.crop(
+                    page_img,
+                    textline.baseline,
+                    textline.heights,
+                    return_mapping=True)
+        line_crop = self.repair_line(line_crop, textline.transcription)
+        page_img = self.cropper.blend_in(page_img, line_crop, line_mapping, offset)
+        return page_img
+
+    def repair_line(self, line, transcription):
+        labels = self._transcriptions_to_labels([transcription])[0]
+        labels = self._clip_or_pad_labels(labels).astype(np.int32)
+
+        line = self._clip_or_pad_image(line)
+        line = line[np.newaxis, :, :, :] / 255.
+
+        feed_dict = {'inference_content:0': line,
+                     'inference_style:0': line,
+                     'inference_transcriptions:0': labels}
+        output = self.repair_session.run('inference_op:0', feed_dict=feed_dict)
+        return (255*np.clip(output[0, :, :, :], 0, 1)).astype(np.uint8)
+
+    def inpaint_line(self, line, transcription):
+        labels = self._transcriptions_to_labels([transcription])[0]
+        labels = self._clip_or_pad_labels(labels).astype(np.int32)
+
+        line = self._clip_or_pad_image(line)
+        line = line[np.newaxis, :, :, :] / 255.
+
+        feed_dict = {'inference_content:0': line,
+                     'inference_style:0': line,
+                     'inference_transcriptions:0': labels}
+        output = self.inpainting_session.run('inference_op:0', feed_dict=feed_dict)
+        return (255*np.clip(output[0, :, :, :], 0, 1)).astype(np.uint8)
+
+    def _transcriptions_to_labels(self, transcriptions):
+        trans = str.maketrans(''.join(self.from_char), ''.join(self.to_char))
+        labels = [np.asarray([ord(x) for x in l.translate(trans)], dtype=np.int32) for l in transcriptions]
+        return labels
+
+    def _clip_or_pad_labels(self, labels):
+        labels_clipped = np.zeros((1, self.max_labels))
+        if labels.shape[0] > self.max_labels:
+            labels_clipped[0, :] = labels[:self.max_labels]
+        else:
+            labels_clipped[0, :labels.shape[0]] = labels
+        return labels_clipped
+
+    def _clip_or_pad_image(self, image):
+        if image.shape[1] > self.max_width:
+            image_clipped = image[:, :self.max_width, :]
+            print('Warning: image line too long, please only attempt repairing lines shorter than {} pixels'.format(self.max_width))
+        else:
+            image_clipped = np.zeros((self.height, self.max_width, 3))
+            image_clipped[:, :image.shape[1], :] = image
+        return image_clipped
+
+
+    def _clip_transcriptions(self, transcriptions, batch_size, max_labels):
+        new_transcriptions = np.zeros((1, max_labels))
+        for i, transcription in enumerate(transcriptions):
+            new_transcription = np.zeros(max_labels)
+            if transcription.shape[0] > max_labels:
+                new_transcription = transcription[:max_labels]
+            else:
+                new_transcription[:transcription.shape[0]] = transcription
+            new_transcriptions[i,:] = new_transcription
+        return new_transcriptions.astype(np.int32)
---
+++ repair_page.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+import shutil
+import time
+import os
+import argparse
+import re
+import configparser
+
+import numpy as np
+import tensorflow as tf
+tf.get_logger().setLevel('INFO')
+import matplotlib.pyplot as plt
+import cv2
+import shapely.geometry
+import tkinter as tk
+
+from pero.document_ocr import layout, page_parser
+import repair_engine
+
+def parseargs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input-images', required=True, help='Input image folder to enhance')
+    parser.add_argument('-o', '--output-path', required=True, help='output image folder to save enhanced images')
+    parser.add_argument('-r', '--repair-json', help='Path to repair engine json', default='./models/LN_enhancement_models/repair_engine.json')
+    parser.add_argument('-x', '--input-page', help='Input page xml folder (if empty or None, automatic line detection and OCR is run', default=None)
+    parser.add_argument('-p', '--parse-config', help='Path to page parser config file', default=None)
+    return parser.parse_args()
+
+def main():
+
+    args = parseargs()
+    if not os.path.isdir(args.output_path):
+        os.makedirs(args.output_path)
+
+    print('Loading engines...')
+    enhancer = repair_engine.EngineRepairCNN(args.repair_json)
+
+    if args.parse_config is not None:
+        config = configparser.ConfigParser()
+        config.read(args.parse_config)
+        # convert relative paths to absolute
+        for section, key in [['LINE_PARSER', 'MODEL_PATH'], ['OCR', 'OCR_JSON']]:
+            if not os.path.isabs(config[section][key]):
+                config[section][key] = os.path.realpath(os.path.join(os.path.dirname(args.parse_config), config[section][key]))
+        parser = page_parser.PageParser(config)
+    else:
+        parser = None
+
+    for filename in os.listdir(args.input_images):
+        page_img = cv2.imread(os.path.join(args.input_images, filename))
+        page_id, _ = os.path.splitext(filename)
+        page_xml_file = os.path.join(args.input_page, page_id+'.xml')
+
+        if os.path.exists(page_xml_file):
+            page_layout = layout.PageLayout(file=page_xml_file)
+        elif not os.path.exists(page_xml_file) and parser is not None:
+            print('Page xml file for page {} not found, running automatic parser...'.format(page_id))
+            page_layout = layout.PageLayout(id=page_id, page_size=(page_img.shape[0], page_img.shape[1]))
+            page_layout = parser.process_page(page_img, page_layout)
+        else:
+            raise Exception('Page xml file for page {} not found and automatic page parser config not specified.'.format(page_id))
+
+        page_img = enhancer.enhance_page(page_img, page_layout)
+
+        cv2.imwrite(os.path.join(args.output_path, '{}_enhanced.jpg'.format(page_id)), page_img)
+
+if __name__=='__main__':
+    main()
